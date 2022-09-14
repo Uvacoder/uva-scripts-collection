ea944f78bbb12eb7101ff462c2110e477d9eded9
Uvacoder <uva22940coder@gmail.com>
2022-09-14 19:17:48 -0400

    Create 01_Variables_DatatTypes_Operators.js

---
+++ tutorials-webdev/JavaScript/01_Variables_DatatTypes_Operators.js
@@ -0,0 +1,27 @@
+// Varibles, Data Types & Operators
+// Varibles(Dynamic Typing)
+var a = 45;
+var b = "Vishal";
+console.log(b);
+
+// Operators In JavaScript
+// 1. Unary Operators - It have single operand. For example:
+a = -a;
+console.log(a);
+// 2. Binary Operators - It have Two operand. For example:
+a = a + 4;
+console.log(a);
+
+var num1 = 45;
+var num2 = 5;
+
+// Arothmetic Operators
+console.log("The Value of num1 + num2 = " + (num1 + num2));
+console.log("The Value of num1 - num2 = " + (num1 - num2));
+console.log("The Value of num1 * num2 = " + (num1 * num2));
+console.log("The Value of num1 / num2 = " + (num1 / num2));
+console.log("The Value of num1 ** num2 = " + (num1 ** num2));
+console.log("The Value of num1++ = " + (num1++));
+console.log("The Value of ++num1 = " + (++num1));
+console.log("The Value of num1-- = " + (num1--));
+console.log("The Value of --num1 = " + (--num1));
