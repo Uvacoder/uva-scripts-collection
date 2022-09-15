c1e4eecb04632e0b145c4d07df2d6109b2c3347d
Uvacoder <uva22940coder@gmail.com>
2022-09-15 02:04:27 -0400

    Add files via upload

---
+++ js-scripts-projects/removecss/removecss.js
@@ -0,0 +1,10 @@
+// Remove CSS
+
+{
+    document.querySelectorAll('style').forEach(item => { item.remove() });
+    document.querySelectorAll('link').forEach(item => { item.remove() });
+    document.querySelectorAll('[style]').forEach(item => { item.removeAttribute('style') });
+
+    var done = 'Finished running “Remove CSS”'
+    done;
+}
